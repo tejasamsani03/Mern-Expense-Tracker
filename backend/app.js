e4578d95336f0a760749becea311fde5f0e0af70
Veerandra Mohan <veeru.sripadam@gmail.com>
2025-11-10 10:56:14 -0500

    updated

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { readdirSync } = require('fs');
+const { db } = require('./db/db');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+
+// CORS Configuration
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    optionsSuccessStatus: 200 // For legacy browser support
+};
+app.use(cors(corsOptions));
+
+// Routes (example)
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
+
+const server = () => {
+    db(); // Connect to database
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT);
+    });
+};
+
+server();
